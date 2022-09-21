278c3dbfb1152402ff6e000093576293b6b3085e
Icalinguaplusplus <Icalinguaplusplus@protonmail.com>
2022-09-21 23:15:09 +0800

    ⚡️ Use referrerpolicy instead of reverse proxy

--- frontend/src/components/MessageItem/ImgMessage.vue
+++ frontend/src/components/MessageItem/ImgMessage.vue
@@ -1,9 +1,10 @@
 <template>
   <a :href="msg.url" class="m-1 w-[fit-content] block" target="_blank" rel="noreferrer">
     <img
-      :src="`/api/cors?url=${msg.url}`"
+      :src="msg.url"
       alt="Chat image"
       class="max-h-[30vh] max-w-[20vw] block"
+      referrerpolicy="no-referrer"
     />
   </a>
 </template>
